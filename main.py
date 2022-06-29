3675af490debb77bd1137617d6729c9bb91d5feb
memojang <99381075+Momejang@users.noreply.github.com>
2022-06-29 05:59:43 +0000

    .

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import requests
 from bs4 import BeautifulSoup as Bs
 print('hello world')
 
-web = requests.get('https://www.daum.net')
+url = 'https://finance.naver.com/item/main.nhn?code=005930'
+url2 = 'https://www.daum.net'
+web = requests.get(url)
 soup = Bs(web.content, 'html.parser')
+pf = soup.select_one('#_nowVal')
 
-print(soup.script)
+#print(soup.script)
+print(pf)
+print('@@@')
 
 
